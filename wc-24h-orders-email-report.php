8defdea449dc13399662484a421fb5f4833c6130
DeepVoid <a.vannini@deepvoid.it>
2026-08-24 00:53:53 +0200

    Aggiornamento di alcune funzioni

--- wc-24h-orders-email-report.php
+++ wc-24h-orders-email-report.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/DeepVoid/wc-24h-orders-email-report
  * Text Domain: woocommerce-24h-orders-email-report
  * Description: Invia automaticamente via email il report degli ordini ricevuti nelle ultime 24 ore, con destinatari e orario configurabili.
- * Version: 1.1.7
+ * Version: 1.1.8
  * Author: Alex Vannini - DeepVoid
  * Requires at least: 6.5
  * Requires PHP: 7.4
@@ -22,7 +22,7 @@ defined( 'ABSPATH' ) || exit;
 
 final class CB_WC_24H_Orders_Email_Report {
 
-	const VERSION    = '1.1.7';
+	const VERSION    = '1.1.8';
 	const AS_GROUP   = 'cb-wc-24h-report';
 	const OPTION_KEY = 'cb_wc_24h_report_settings';
 	const CRON_HOOK  = 'cb_wc_24h_report_send';
